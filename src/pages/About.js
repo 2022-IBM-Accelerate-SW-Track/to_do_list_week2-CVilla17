83d6e0aff24e013c21ea4aba10f6b4a2d3047a34
CVilla17 <villac@mit.edu>
2022-06-15 19:26:09 -0400

    my about page and pic change in assets

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,10 +18,23 @@ export default class About extends Component {
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Carlos Villa</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              <div>Hi there and welcome!</div>
+              <div>
+                Here's some fun facts:
+                <ul>
+                  <li>I'm a rising Sophomore at MIT over in Cambridge</li>
+                  <li>I was born in Illinois but my family is from Mexico</li>
+                  <li>
+                    I'm studying Electrical Engineering and Computer Science
+                  </li>
+                  <li>
+                    Some of my goals include innovating, educating, and bringing
+                    communities together!
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
